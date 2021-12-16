a39ed71347a290430aca89125fd4e92112c9be7b
Deeksha <gldeeksha@gmail.com>
2021-12-17 03:01:55 +0530

    Database migrated

--- app.py
+++ app.py
@@ -2,43 +2,87 @@ from flask import Flask, render_template, request
 import json
 from flask_mysqldb import MySQL
 import yaml
+import pandas as pd
+from flask_sqlalchemy import SQLAlchemy
+from numpy import genfromtxt
+import csv
+from sqlalchemy.orm import sessionmaker
 
 app=Flask(__name__)
 
-db=yaml.safe_load(open('db.yaml'))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@127.0.0.1:3306/flask_app_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-# app.config['MySQL_HOST']='127.0.0.1'
-# app.config['MySQL_USER']='root'
-# app.config['MySQL_PASSWORD']='root'
-# app.config['MySQL_DB']='flaskapp'
-app.config['MySQL_HOST']=db['mysqlHost']
-app.config['MySQL_USER']=db['mysqlUser']
-app.config['MySQL_PASSWORD']=db['mysqlPassword']
-app.config['MySQL_DB']=db['mysqlDb']
-
-mysql=MySQL(app)
+class Banks(db.Model):
+    __tablename__="banks"
+    bank_name=db.Column(db.String(49))
+    bank_id=db.Column(db.BigInteger, nullable=False, primary_key=True)
+    branch = db.relationship('Branches')
+    def __repr__(self) -> str:
+        return "<{}:{}>"
 
 
-#Given a bank branch IFSC code, get branch details
-@app.route('/ifsc/<ifsc_code>')
-def ifsc_get(ifsc_code):
-	conn = mysql.connection.cursor()
-	cur = conn.cursor()
-	cur.execute("SELECT * FROM bank_branches where ifsc=?;", (ifsc_code,) )
-	branch = cur.fetchall()
-	conn.commit()
-	return json.dumps(branch)
+class Branches(db.Model):
+    __tablename__="branches"
+    ifsc= db.Column(db.String(11), nullable= False,primary_key=True)
+    bank_id=db.Column(db.BigInteger, db.ForeignKey('banks.bank_id'))
+    branch = db.Column(db.String(74), unique=False)
+    address = db.Column(db.String(195), unique=False)
+    city = db.Column(db.String(50), unique=False)
+    district = db.Column(db.String(50), unique=False)
+    state = db.Column(db.String(26), unique=False)
 
-#Given a bank name and city, gets details of all branches of the bank in the city
-@app.route('/bank_name/<bank_name>/city/<city>')
-def branch_city_get(bank_name,city):
-	conn = mysql.connection.cursor()
-	cur = conn.cursor()
-	cur.execute("SELECT * FROM bank_branches where bank_name=? and city=?;", (bank_name,city,) )
-	branches = cur.fetchall()
-	conn.commit()
-	return json.dumps(branches)
+# class bank_branches(mysql.Model):
 
-if __name__=='__main__':
-	app.run(debug=True)
+df = pd.read_csv('bank_branches.csv', delimiter=',')
+
+#specified columns we want to import and covert it to list
+columns=df.columns
+df_data=df[columns]
+records=df_data.values.tolist()
+temp=records[1]
+# db.banks.bank_name=temp[7]
+
+# abc = banks(temp[7], temp[1])
+# db.session.add(abc)
+# db.session.commit()
+   
+# @app.route('/populate_data' , methods=['POST'])
+# def populate_data():
+#     conn=db.connect()
+#     conn.cursor()
+#     with open('bank_branches.csv','rb') as f:
+#         df=pd.read_csv(f,na_filter=False)
+#         bank_dict=df.to_dict(orient='records')
+#         for bank in bank_dict:
+
+
+# Given a bank branch IFSC code, get branch details
+# @app.route('/ifsc/<ifsc_code>')
+# def ifsc_get(ifsc_code):
+# 	conn = mysql.connection.cursor()
+# 	cur = conn.cursor()
+# 	cur.execute("SELECT * FROM bank_branches where ifsc=?;", (ifsc_code,) )
+# 	branch = cur.fetchall()
+# 	conn.commit()
+# 	return json.dumps(branch)
+
+# #Given a bank name and city, gets details of all branches of the bank in the city
+# @app.route('/bank_name/<bank_name>/city/<city>')
+# def branch_city_get(bank_name,city):
+# 	conn = mysql.connection.cursor()
+# 	cur = conn.cursor()
+# 	cur.execute("SELECT * FROM bank_branches where bank_name=? and city=?;", (bank_name,city,) )
+# 	branches = cur.fetchall()
+# 	conn.commit()
+# 	return json.dumps(branches)
+
+# if __name__=='__main__':
+# 	app.run(debug=True)
+
+
+
+
+
 
--- data.py
+++ data.py
@@ -7,33 +7,34 @@ df = pd.read_csv('bank_branches.csv', delimiter=',')
 columns=df.columns
 df_data=df[columns]
 records=df_data.values.tolist()
+print(records[1])
 
 #creating MySQL server connection
 
-app=Flask(__name__)
+# app=Flask(__name__)
 
-app.config['MySQL_HOST']='127.0.0.1'
-app.config['MySQL_USER']='root'
-app.config['MySQL_PASSWORD']='root'
-app.config['MySQL_DB']='flaskapp'
+# app.config['MySQL_HOST']='127.0.0.1'
+# app.config['MySQL_USER']='root'
+# app.config['MySQL_PASSWORD']='root'
+# app.config['MySQL_DB']='flaskapp'
 
-mysql=MySQL(app)
-print(mysql)
-print(mysql.connection)
-
-
-# @app.route('/')
-# def index():
-#     cur=mysql.connection.cursor()
-# if __name__ == '__main__':
-#     app.run()
-
+# mysql=MySQL(app)
 # print(mysql)
-# query='INSERT INTO bank_branches(ifsc,bank_id,branch,address,city,district,state,bank_name)' 'values(%s,%s,%s,%s,%s,%s,%s,%s)'
-# conn = mysql.cursor()
-# # conn.executemany(query,records)
-# # mysql.connection.commit()
-# # conn.close()
+# print(mysql.connection)
 
-# db = mysql.connect('localhost', 'root', 'password', 'db')
-# cursor = db.cursor()
+
+# # @app.route('/')
+# # def index():
+# #     cur=mysql.connection.cursor()
+# # if __name__ == '__main__':
+# #     app.run()
+
+# # print(mysql)
+# # query='INSERT INTO bank_branches(ifsc,bank_id,branch,address,city,district,state,bank_name)' 'values(%s,%s,%s,%s,%s,%s,%s,%s)'
+# # conn = mysql.cursor()
+# # # conn.executemany(query,records)
+# # # mysql.connection.commit()
+# # # conn.close()
+
+# # db = mysql.connect('localhost', 'root', 'password', 'db')
+# # cursor = db.cursor()
